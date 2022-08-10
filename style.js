5493f7c878f071e01e97d9c49ec7ffc6b52fbb27
Natnael Shawel <natnaelshaw19@gmail.com>
2022-08-10 12:53:08 -0700

    Styling DOM Elements

---
+++ style.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('style');
+ header.style.color = '#39a0ca';
+ const body = document.getElementById('message');
+ body.style.fontStyle='italic';
+ body.style.fontsize=400;
